1db8028d6ce8a9020e958ecd055c1f6e094fc0b9
easylog <easyloghilfe@gmail.com>
2025-04-24 08:31:23 +0200

    Delete pages directory

--- pages/dashboard/staff.js
+++
@@ -1,8 +0,0 @@
-export default function StaffDashboard() {
-  return (
-    <main style={{ padding: '2rem' }}>
-      <h1>📋 Mitarbeiter-Dashboard</h1>
-      <p>Willkommen! Du kannst jetzt Journal und ChangeBoard nutzen.</p>
-    </main>
-  );
-}
--- pages/journal.js
+++
@@ -1,8 +0,0 @@
-export default function Journal() {
-  return (
-    <main style={{ padding: '2rem' }}>
-      <h1>📝 GPT-Journal</h1>
-      <p>Hier kannst du dein Journal schreiben – ohne Login!</p>
-    </main>
-  );
-}
